e1118e9d4145260c77f94aa8a8dd272abfdbe7a8
Eli <eli.davis1104@gmail.com>
2021-10-01 12:27:01 -0400

    added basic functionality

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
 
 
-
-
-
-
-
-
-// Part 1 of exercise
-
 const add = function(a, b) {
     return a + b
   
@@ -45,7 +37,6 @@ const add = function(a, b) {
 
   let operationChoice = '';
   let userInput = '';
-  let storeInput = false;
   let firstOperand = 0   ;
   let secondOperand= "";
 
@@ -74,7 +65,7 @@ const add = function(a, b) {
     // it receives the input of the user
     // and triggers the corresponding function to it
     if (userInput == '=') {
-      
+      handleEqual();
     } else if (userInput == 'DELETE'){
       deleteText();
     } else if (userInput == 'CLEAR') {
@@ -99,80 +90,109 @@ const add = function(a, b) {
   }
   const deleteText = function(){
     // removes the last digit of input
-    if (bottomDisplay.textContent.length != '1')
+    if (containsEqual(topText) || containsOperator(topText))
+      {resetCalc()
+      } else if  (bottomDisplay.textContent.length != '1')
       {bottomText = bottomText.slice(0, (bottomText.length - 1))
-      } else {bottomText = '0'}
+      } 
+      else {bottomText = '0'}
     }
-       
 
   const updateScreen = function() {
     bottomDisplay.textContent = bottomText;
     setTopText();
     topDisplay.textContent= topText;
   }
-  // const handleOperators = function() {
-  //   if (topText == ''){
-  //     topText = bottomText + userInput;
-  //     operationChoice = userInput;
-  //   } else if (!containsOperator()) {
+  const handleOperators = function() {
+    if (topText== ''){
+      topText = bottomText + userInput;
+      operationChoice = userInput;
+    } else if (containsEqual(topText) ){
+      topText = bottomText + userInput;      
+      operationChoice = userInput;
+    }
+      else if (containsOperator(topText) && secondOperand!== ''){
+        if (operate(firstOperand, secondOperand, operationChoice))
+         {bottomText = operate(firstOperand, secondOperand, operationChoice);
+          firstOperand = bottomText;  
+          secondOperand = '';
+          operationChoice = userInput;
+          topText = bottomText + operationChoice;
+        }
       
-  //   } else if (containsOperator()){}
-  //   console.log(topText);
-  //   bottomText = operate(firstOperand, secondOperand, userInput);
-  // } 
+      
+    } 
+    
+  } 
     
   
   const handleNumbers = function() {
     if (bottomText == '0') {
       bottomText = userInput
     } else {bottomText += userInput}
-    if (!containsOperator() ||containsEqual()) {
+    if (!containsOperator(topText) ||containsEqual()) {
       firstOperand += userInput
-    } else if (containsOperator() && !containsEqual())
+    } else if (containsOperator(topText) && !containsEqual(topText))
      {secondOperand += userInput
       bottomText= secondOperand}
   } 
 
 
-  function containsOperator() {
+  function containsOperator(text) {
     // loops through the inputted str seeing if operator includes it's current index val
-    for (let i = 0 ; i < topText.length; i ++) {
-      if (operators.includes(topText[i])) {return true}}
+    for (let i = 0 ; i < text.length; i ++) {
+      if (operators.includes(text[i])) {return true}}
     return false};
   function containsEqual() {
     // loops through the inputted str seeing if operator includes it's current index val
     return topText.includes('=') }
 
   const handleEqual = function() {
-    if (!containsOperator()) {
-    } else if (containsOperator() && secondOperand !== ""){
-      operate(a, b, operationChoice)
+    if (containsOperator(topText) && !containsEqual(topText)) {
+      topText = topText + secondOperand + "=";
+      result = operate(firstOperand, secondOperand, operationChoice);
+      bottomText= result;
+      firstOperand = result;
+      secondOperand = '';
+    } 
     }
-  }
+  
   
 
   function findOperatorIndex(str) {
     for (let i = 0 ; i < str.length; i ++) {
       if (operators.includes(str[i])) {return i}}
   }
-  function setTopText() {
-    if (containsOperator()){
-      let pos = findOperatorIndex(topText);
-      topText = topText.slice(0, pos) + " " + topText.slice(pos, topText.length);
-      
-    }
-
+  function findEqualIndex(str) {
+    for (let i = 0 ; i < str.length; i ++) {
+      if ('='.includes(str[i])) {return i}}
   }
+  function setTopText() {
+    if (containsOperator(topText)){
+      let pos = findOperatorIndex(topText);
+      topText = topText.slice(0, pos) + " " + topText.slice(pos, pos +1) + " " + topText.slice(pos +1, topText.length);
+    }
+    if (containsEqual(topText))  {
+      let pos = findEqualIndex(topText);
+      topText = topText.slice(0, pos) + " " + topText.slice(pos, topText.length);
+    } 
+  }
+
+  
 
   const operate = function(a, b, operator) {
     if (operator == "+") {
-        return add(a,b);
+        return add(parseInt(a),parseInt(b));
     } else if (operator == "-") {
       return subtract(a, b);
     } else if (operator == 'x') {
       return multiply(a,b);
+    } else if(operator == "÷" && b == '0') {
+      alert("You can't divide by Zero, silly.");
+      resetCalc();
+      return false;
     } else if (operator == "÷" && b !== 0) {
-      return divide(a,b);
-    } else {
-      return "error unsupported operation";
-    }}
+        return divide(a,b);
+    }
+    }
+    
